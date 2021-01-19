e709a4960373b909e9a79a0d76514b742818e8d8
colin-evers <evers@nyc.rr.com>
2021-01-18 19:32:53 -0500

    func subset a

--- C0.py
+++ C0.py
@@ -3,4 +3,6 @@ C0
 #Initial commit - skeleton layout 
 #Interface to feature set 1
 #Interface to feature set 2
-# starting point for feature access
+#starting point for feature access
+
+#functionality subset a 
